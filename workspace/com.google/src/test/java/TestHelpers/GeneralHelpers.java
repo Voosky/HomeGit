f57b57a0e8aa025a2a61e42b3ccead812543f998
Voosky <vuscan_razvan@yahoo.com>
2013-03-23 13:55:18 +0200

    *Added a method

---
+++ workspace/com.google/src/test/java/TestHelpers/GeneralHelpers.java
@@ -0,0 +1,5 @@
+package TestHelpers;
+
+public class GeneralHelpers {
+
+}
--- workspace/com.google/src/test/java/TestHelpers/MethodsToBeUsed.java
+++ workspace/com.google/src/test/java/TestHelpers/MethodsToBeUsed.java
@@ -1,5 +1,11 @@
 package TestHelpers;
 
+import org.openqa.selenium.WebDriver;
+
 public class MethodsToBeUsed {
 //methods used in the main program are stored here.
+	
+	public static void setUsername(String username, WebDriver driver) throws Exception {
+		//add username
+	}
 }
